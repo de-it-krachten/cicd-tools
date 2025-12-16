ce54448a0f63b28ecac635588443fd02e23ba529
Mark van Huijstee <Mark.van.Huijstee@de-it-krachten.nl>
2025-12-16 16:31:01 +0100

    feat: Add ansible-navigator (in venv)

--- scripts/python/python.sh
+++ scripts/python/python.sh
@@ -82,7 +82,6 @@ function Setup_venv
 
   $sudo $python -m venv $venv || exit 1
   $sudo $venv/bin/pip3 install pip wheel setuptools setuptools_rust --upgrade || exit 1
-  $sudo $venv/bin/pip3 list
 
 }
 
@@ -90,8 +89,14 @@ function Get_key
 {
 
   local key=$1
+  local subkey=$2
 
-  yq -y $key $Configfile | sed '/\.\.\./d;/null/d;/\[\]/d;s/^- //'
+  if [[ -z $subkey ]]
+  then
+    yq -y '."'$key'"' $Configfile | sed '/\.\.\./d;/null/d;/\[\]/d;s/^- //'
+  else
+    yq -y '."'$key'".'$subkey'' $Configfile | sed '/\.\.\./d;/null/d;/\[\]/d;s/^- //'
+  fi
 
 }
 
@@ -179,16 +184,16 @@ Setup
 Template ${Configfile}.j2
 
 # Get defaults
-Python_default=$(Get_key .default.python)
+Python_default=$(Get_key default python)
 
 # Check that profile exists
-profile=$(Get_key .$Profile)
+profile=$(Get_key $Profile)
 [[ -z $profile ]] && echo "Profile '$Profile' does not exist" >&2 && exit 1
 
 # Get list of packages
-Pip_packages1=$(Get_key .generic.packages)
-Pip_packages2=$(Get_key .$Profile.packages)
-Python=$(Get_key .$Profile.python)
+Pip_packages1=$(Get_key generic packages)
+Pip_packages2=$(Get_key $Profile packages)
+Python=$(Get_key $Profile python)
 Python=${Python:-$Python_default}
 Python=${Python:-`readlink -f /usr/bin/python3`}
 
@@ -216,25 +221,24 @@ echo "$Pip_packages1" | sed "/^#/d" > ${TMPFILE}1
 echo "$Pip_packages2" | sed "/^#/d" > ${TMPFILE}2
 
 # Install pypi packages
-if [[ $Verbse == true ]]
+if [[ $Verbose == true ]]
 then
   $Venv/bin/pip3 install -r ${TMPFILE}1 || exit 1
   [[ -n $Pip_packages2 ]] && $Venv/bin/pip3 install -r ${TMPFILE}2
-  $Venv/bin/pip3 list
 else
   $Venv/bin/pip3 install -r ${TMPFILE}1 >/dev/null || exit 1
   [[ -n $Pip_packages2 ]] && $Venv/bin/pip3 install -r ${TMPFILE}2 >/dev/null
 fi
 
 # Setup symlinks
-symlinks=$(yq -y .$Profile.links $Configfile | sed '/\.\.\./d;/null/d;/\[\]/d;s/^- //')
+symlinks=$(yq -y '."'$Profile'".links' $Configfile | sed '/\.\.\./d;/null/d;/\[\]/d;s/^- //')
 for symlink in $symlinks
 do
   ln -fs $Venv/bin/$symlink /usr/local/bin/$symlink
 done
 
 # Install requirements
-reqs=$(yq -y .$Profile.requirements $Configfile | sed '/\.\.\./d;/null/d;/\[\]/d;s/^- //')
+reqs=$(yq -y .'"'$Profile'".requirements' $Configfile | sed '/\.\.\./d;/null/d;/\[\]/d;s/^- //')
 for req in $reqs
 do
   reqfile=$Venv/lib/$(basename $Python)/site-packages/ansible_collections/$req
@@ -242,3 +246,6 @@ do
   [[ -f $reqfile && $Verbose == false ]] && $Venv/bin/pip3 install -r $reqfile >/dev/null
 
 done
+
+# Show result
+[[ $Verbose == true ]] && $Venv/bin/pip3 list
