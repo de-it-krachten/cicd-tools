98191bfde36b5045c1d221db266850411e4d0efd
Mark van Huijstee <Mark.van.Huijstee@de-it-krachten.nl>
2026-03-04 21:48:19 +0100

    feat: Add support for support changes

---
+++ scripts/cicd/ci-platform-support.sh
@@ -0,0 +1,317 @@
+#!/bin/bash
+
+##############################################################
+#
+# Defining standard variables
+#
+##############################################################
+
+# Set temporary PATH
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH
+
+# Get the name of the calling script
+FILENAME=$(readlink -f $0)
+BASENAME="${FILENAME##*/}"
+BASENAME_ROOT=${BASENAME%%.*}
+DIRNAME="${FILENAME%/*}"
+
+# Get name of symlink used to execute
+FILENAME1=$(realpath -s $0)
+BASENAME1="${FILENAME1##*/}"
+BASENAME1_ROOT=${BASENAME1%%.*}
+DIRNAME1="${FILENAME1%/*}"
+
+# Define temorary files, debug direcotory, config and lock file
+TMPDIR=$(mktemp -d)
+VARTMPDIR=/var/tmp
+TMPFILE=${TMPDIR}/${BASENAME}.${RANDOM}.${RANDOM}
+DEBUGDIR=${TMPDIR}/${BASENAME_ROOT}_${USER}
+CONFIGFILE=${DIRNAME}/${BASENAME_ROOT}.cfg
+LOCKFILE=${VARTMP}/${BASENAME_ROOT}.lck
+
+# Logfile & directory
+LOGDIR=$DIRNAME
+LOGFILE=${LOGDIR}/${BASENAME_ROOT}.log
+
+# Set date/time related variables
+DATESTAMP=$(date "+%Y%m%d")
+TIMESTAMP=$(date "+%Y%m%d.%H%M%S")
+
+# Figure out the platform
+OS=$(uname -s)
+
+# Get the hostname
+HOSTNAME=$(hostname -s)
+
+
+##############################################################
+#
+# Defining custom variables
+#
+##############################################################
+
+CSVFILE=~/role-support.csv
+ROLE=$(basename $PWD)
+
+
+##############################################################
+#
+# Defining standardized functions
+#
+#############################################################
+
+# FUNCTIONS=${DIRNAME}/functions.sh
+# if [[ -f ${FUNCTIONS} ]]
+# then
+#    . ${FUNCTIONS}
+# else
+#    echo "Functions file '${FUNCTIONS}' could not be found!" >&2
+#    exit 1
+# fi
+
+
+##############################################################
+#
+# Defining customized functions
+#
+#############################################################
+
+function Usage
+{
+
+  cat << EOF | grep -v "^#"
+
+$BASENAME
+
+Usage : $BASENAME <flags> <arguments>
+
+Flags :
+
+   -d|--debug           : Debug mode (set -x)
+   -D|--dry-run         : Dry run mode
+   -h|--help            : Prints this help message
+   -v|--verbose         : Verbose output
+
+   -c|--commit          : Create commit for changed in support
+   -o|--cicd-overwrite  : Update .cicd.overwrite file
+   -s|--snapshot        : Create support snapshot file
+
+EOF
+
+}
+
+function Get_headers
+{
+
+  # Convert TSV -> CSV
+  sed -i "s/\t/;/g" $CSVFILE
+  sed -i "s/^role;/name;/" $CSVFILE
+
+  # Get headers
+  Headers=$(awk 'NR==1' $CSVFILE | sed "s/;/ /g")
+  xHeaders=$(awk 'NR==1' $CSVFILE | sed "s/;/ /g" | sed -r "s/([a-z0-9]+)/x\\1/g")
+
+  # Get defaults
+  IFS=";"
+  eval read $Headers <<< "$(awk 'NR==2' $CSVFILE)"
+  eval read $xHeaders <<< "$(awk -F';' '$1=="'$ROLE'" {print $0}' $CSVFILE)"
+  unset IFS
+
+}
+
+function Cicd_overwrite
+{
+
+  # Create platfrom support overview
+  for Header in $Headers
+  do
+
+    [[ $Header == name ]] && continue
+
+    eval Value1=\$$Header
+    eval Value2=\$x$Header
+
+    [[ $Value1 == $Value2 ]] && continue
+
+    [[ $first != false ]] && echo -e "\n# Supported platforms\nplatforms:"
+
+    support=
+    ci=
+
+    case $Value2 in
+      Y)
+        support=true
+        ci=true
+        ;;
+      y)
+        support=true
+        ci=false
+        ;;
+      N)
+        support=false
+        ci=false
+        ;;
+      x)
+        echo "Warning: $Header is not defined!" >&2
+        ;;
+      *)
+        echo "Unsupported value '$Value2' found" >&2
+        exit 1
+        ;;
+    esac
+
+    cat <<EOF
+  $Header:
+    supported: $support
+    ci: $ci
+EOF
+
+    first=false
+    counter=$(($counter+1))
+
+  done > $TMPFILE
+
+  # Append empty line
+  [[ -s $TMPFILE ]] && echo >> $TMPFILE
+
+  # Delete any platforms present
+  sed -i '/# Supported platforms/,/^$/d' .cicd.overwrite
+  sed -i '/platforms:/,/^$/d' .cicd.overwrite
+
+  # Create supported platforms
+  [[ -s $TMPFILE ]] && sed -i -e '/^generic:/,/^$/ { /^$/ { r '$TMPFILE'' -e 'd; }; }' .cicd.overwrite
+
+  # Remove multiple empty lines 
+  sed -i '/^$/N;/\n$/D' .cicd.overwrite
+
+}
+
+function Snapshot
+{
+
+  yq -s '.[0] * .[1]' .cicd .cicd.overwrite | \
+  yq -r '.platforms | to_entries | .[] | [.key, .value.name, .value.ci, .value.supported] | @csv' | \
+  sed "s/,/|/g" | \
+  sed "s/\"//g"
+
+}
+
+function Commit
+{
+
+  # Move snapshot files
+  mv .snapshot1 ${TMPFILE}snapshot1
+  mv .snapshot2 ${TMPFILE}snapshot2
+
+  # Commit all files
+  git status
+  git add .
+  git commit -m "Update CI"
+
+  for Distro in $Headers
+  do
+
+    Pre=$(awk -F'|' '$1=="'$Distro'" {print $4}' ${TMPFILE}snapshot1)
+    Post=$(awk -F'|' '$1=="'$Distro'" {print $4}' ${TMPFILE}snapshot2)
+    if [[ $Pre != $Post ]]
+    then
+      Os=$(awk -F'|' '$1=="'$Distro'" {print $2}' ${TMPFILE}snapshot2)
+      if [[ $Post == true ]]
+      then
+        git commit -am "feat: Added support for $Os" --allow-empty
+      else
+        git commit -am "feat: Removed support for $Os" --allow-empty
+      fi
+    fi
+
+  done
+
+}
+
+##############################################################
+#
+# Main programs
+#
+#############################################################
+
+# Make sure temporary files are cleaned at exit
+trap 'rm -fr ${TMPDIR}' EXIT
+trap 'exit 1' HUP QUIT KILL TERM INT
+
+# Set the defaults
+Debug_level=0
+Verbose=false
+Verbose_level=0
+Dry_run=false
+Echo=
+
+# parse command line into arguments and check results of parsing
+while getopts :cdDhosv-: OPT
+do
+
+  # Support long options
+  if [[ $OPT = "-" ]] ; then
+    OPT="${OPTARG%%=*}"       # extract long option name
+    OPTARG="${OPTARG#$OPT}"   # extract long option argument (may be empty)
+    OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
+  fi
+
+  case $OPT in
+    c|commit)
+      Mode=commits
+      ;; 
+    d|debug)
+      Verbose=true
+      Verbose1="-v"
+      set -vx
+      ;;
+    D|dry-run)
+      Dry_run=true
+      Dry_run1="-D"
+      Echo=echo
+      ;;
+    h|help)
+      Usage
+      exit 0
+      ;;
+    o|cicd-overwrite)
+      Mode=cicd-overwrite
+      ;;
+    s|snapshot)
+      Mode=snapshot
+      ;;
+    v|verbose)
+      Verbose=true
+      Verbose1="-v"
+      ;;
+    *)
+      echo "Unknown flag -$OPT given!" >&2
+      exit 1
+      ;;
+  esac
+
+  # Set flag to be use by Test_flag
+  # eval ${OPT}flag=1
+
+done
+shift $(($OPTIND -1))
+
+if [[ ! -f $CSVFILE ]]
+then
+  echo "Role support matrix '$CSVFILE' could not be found!" >&2
+  exit 1
+fi
+
+case $Mode in
+  commits)
+    Get_headers
+    Commit
+    ;;
+  snapshot)
+    Snapshot
+    ;;
+  cicd-overwrite)
+    Get_headers
+    Cicd_overwrite
+    ;; 
+esac
--- scripts/cicd/ci-wrapper.sh
+++ scripts/cicd/ci-wrapper.sh
@@ -1,4 +1,140 @@
-#!/bin/bash -e
+#!/bin/bash
+
+##############################################################
+#
+# Defining standard variables
+#
+##############################################################
+
+# Set temporary PATH
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH
+
+# Get the name of the calling script
+FILENAME=$(readlink -f $0)
+BASENAME="${FILENAME##*/}"
+BASENAME_ROOT=${BASENAME%%.*}
+DIRNAME="${FILENAME%/*}"
+
+# Get name of symlink used to execute
+FILENAME1=$(realpath -s $0)
+BASENAME1="${FILENAME1##*/}"
+BASENAME1_ROOT=${BASENAME1%%.*}
+DIRNAME1="${FILENAME1%/*}"
+
+# Define temorary files, debug direcotory, config and lock file
+TMPDIR=$(mktemp -d)
+VARTMPDIR=/var/tmp
+TMPFILE=${TMPDIR}/${BASENAME}.${RANDOM}.${RANDOM}
+DEBUGDIR=${TMPDIR}/${BASENAME_ROOT}_${USER}
+CONFIGFILE=${DIRNAME}/${BASENAME_ROOT}.cfg
+LOCKFILE=${VARTMP}/${BASENAME_ROOT}.lck
+
+# Logfile & directory
+LOGDIR=$DIRNAME
+LOGFILE=${LOGDIR}/${BASENAME_ROOT}.log
+
+# Set date/time related variables
+DATESTAMP=$(date "+%Y%m%d")
+TIMESTAMP=$(date "+%Y%m%d.%H%M%S")
+
+# Figure out the platform
+OS=$(uname -s)
+
+# Get the hostname
+HOSTNAME=$(hostname -s)
+
+
+##############################################################
+#
+# Defining custom variables
+#
+##############################################################
+
+
+##############################################################
+#
+# Defining standardized functions
+#
+#############################################################
+
+# FUNCTIONS=${DIRNAME}/functions.sh
+# if [[ -f ${FUNCTIONS} ]]
+# then
+#    . ${FUNCTIONS}
+# else
+#    echo "Functions file '${FUNCTIONS}' could not be found!" >&2
+#    exit 1
+# fi
+
+
+##############################################################
+#
+# Defining customized functions
+#
+#############################################################
+
+function Usage
+{
+
+  cat << EOF | grep -v "^#"
+
+$BASENAME
+
+Usage : $BASENAME <flags> <arguments>
+
+Flags :
+
+   -d|--debug           : Debug mode (set -x)
+   -D|--dry-run         : Dry run mode
+   -h|--help            : Prints this help message
+   -v|--verbose         : Verbose output
+
+EOF
+
+}
+
+function Readme
+{
+
+  # README.md
+  echo "** README"
+  if [[ -f README.md ]] ; then
+    lines=$(cat README.md | wc -l)
+    if grep -q "^## Platforms" README.md ; then
+      echo "Updating generic README"
+      /opt/cicd-tools/bin/readme.sh
+    elif [[ $lines == 1 ]]
+    then
+      echo "Updating generic README"
+      /opt/cicd-tools/bin/readme.sh
+    else
+      echo "Renaming custom README.md -> README-original.md"
+      mv README.md README-original.md
+      /opt/cicd-tools/bin/readme.sh
+    fi
+  else
+    echo "Creating new README.md (none present)"
+    /opt/cicd-tools/bin/readme.sh
+  fi
+
+}
+
+##############################################################
+#
+# Main programs
+#
+#############################################################
+
+# Make sure temporary files are cleaned at exit
+trap 'rm -fr ${TMPDIR}' EXIT
+trap 'exit 1' HUP QUIT KILL TERM INT
+
+# Set the defaults
+Debug_level=0
+Verbose=false
+Verbose_level=0
+Dry_run=false
+Echo=
 
 Phase=all
 Silent=false
@@ -20,6 +156,10 @@ do
     d|debug)
       set -vx
       ;;
+    h|help)
+      Usage
+      exit 0
+      ;;
     p|phase)
       Phase=$OPTARG
       ;;
@@ -36,7 +176,7 @@ do
   esac
 
   # Set flag to be use by Test_flag
-  eval ${OPT}flag=1
+  # eval ${OPT}flag=1
 
 done
 shift $(($OPTIND -1))
@@ -62,48 +202,49 @@ case $repo in
     echo "Ansible role repo '$repo'"
     [[ ! -s .gitignore ]] && touch .gitignore
     sed -i -r "s|^(molecule/default/molecule.yml)$|#\\1|" .gitignore
+
+    # Create support snapshot @start
+    [[ ! -s .snapshot1 ]] && ${DIRNAME}/ci-platform-support.sh --snapshot > .snapshot1
+
+    # Update .cicd file
     /opt/cicd-tools/bin/ci-init.sh $Args -m role -iF
     [[ $Phase == 1 ]] && exit 0
+
+    # Update .cicd.overwrite file
+    ${DIRNAME}/ci-platform-support.sh --cicd-overwrite 
+
+    # Update all CI code
     /opt/cicd-tools/bin/ci-init.sh $Args -m role
+
+    # Create support snapshot @end
+    ${DIRNAME}/ci-platform-support.sh --snapshot > .snapshot2
+
+    # Update README
+    Readme
+
+    # Commit all support changes
+    ${DIRNAME}/ci-platform-support.sh --commit
+
     ;;
   ansible-playbooks-*)
     echo "Ansible playbook repo '$repo'"
     /opt/cicd-tools/bin/ci-init.sh $Args -m playbook -iF
     [[ $Phase == 1 ]] && exit 0
     /opt/cicd-tools/bin/ci-init.sh $Args -m playbook
+    Readme
     ;;
   ansible-collection-*)
     echo "Ansible collection repo '$repo'"
     /opt/cicd-tools/bin/ci-init.sh $Args -m collection -iF
     [[ $Phase == 1 ]] && exit 0
     /opt/cicd-tools/bin/ci-init.sh $Args -m collection
+    Readme
     ;;
   *)
     echo "Unable to figure out what we are dealing with!" >&2
     exit 1
     ;;
 esac
-
-# README.md
-echo "** README"
-if [[ -f README.md ]] ; then
-  lines=$(cat README.md | wc -l)
-  if grep -q "^## Platforms" README.md ; then
-    echo "Updating generic README"
-    /opt/cicd-tools/bin/readme.sh
-  elif [[ $lines == 1 ]]
-  then
-    echo "Updating generic README"
-    /opt/cicd-tools/bin/readme.sh
-  else
-    echo "Renaming custom README.md -> README-original.md"
-    mv README.md README-original.md
-    /opt/cicd-tools/bin/readme.sh
-  fi
-else
-  echo "Creating new README.md (none present)"
-  /opt/cicd-tools/bin/readme.sh
-fi
 
 # Enable Github CI
 set +e
