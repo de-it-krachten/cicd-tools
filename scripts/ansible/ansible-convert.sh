e920f1a065de486b0f718e62f17e96ff7983cd5c
Mark van Huijstee <mark.van.huijstee@de-it-krachten.nl>
2026-07-31 13:28:06 +0200

    fix: Extend list of facts to conver

--- scripts/ansible/ansible-convert.sh
+++ scripts/ansible/ansible-convert.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 vars="
+ansible_architecture
 ansible_distribution
 ansible_os_family
 ansible_env
@@ -13,15 +14,19 @@ ansible_interfaces
 ansible_default_ipv4
 ansible_default_ipv6
 ansible_fqdn
+ansible_hostname
 ansible_os_product_id
 ansible_os_product_key
 ansible_os_license_edition
 ansible_os_license_status
+ansible_processor_vcpus
+ansible_memtotal_mb
+ansible_all_ipv4_addresses
 "
 
 if [[ $# -eq 0 ]]
 then
-  files=$(find tasks handlers defaults vars -type f 2>/dev/null)
+  files=$(find tasks handlers defaults vars templates molecule -type f 2>/dev/null)
 else
   files="$@"
 fi
